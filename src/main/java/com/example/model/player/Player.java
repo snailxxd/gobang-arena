370a850665f7c9986e47e14209dcf2f76d384ebf
snailxxd <2559572532@qq.com>
2025-12-25 11:22:01 +0800

    feat: add private attributes id and roomId

--- src/main/java/com/example/model/player/Player.java
+++ src/main/java/com/example/model/player/Player.java
@@ -3,11 +3,22 @@ package com.example.model.player;
 import com.example.model.room.Stone;
 
 public class Player {
+    private String id;
     private String name;
+    private String roomId;
     private Stone stone;
 
-    Player(String name) {
+    public Player(String id, String name) {
+        this.id = id;
         this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -24,5 +35,13 @@ public class Player {
 
     public void setStone(Stone stone) {
         this.stone = stone;
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
     }
 }
