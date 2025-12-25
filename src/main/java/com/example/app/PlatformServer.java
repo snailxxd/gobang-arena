da6269654dde51ce183b05424b3eadc3af6c2739
snailxxd <2559572532@qq.com>
2025-12-25 15:05:53 +0800

    feat: add PlatformServer

---
+++ src/main/java/com/example/app/PlatformServer.java
@@ -0,0 +1,24 @@
+package com.example.app;
+
+import com.example.service.server.ClientHandler;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static com.example.utils.Utils.*;
+
+public class PlatformServer {
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
+            System.out.println("Server started at port " + serverSocket.getLocalPort());
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("Client connected");
+                new Thread(new ClientHandler(socket)).start();
+            }
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: " + SERVER_PORT);
+        }
+    }
+}
