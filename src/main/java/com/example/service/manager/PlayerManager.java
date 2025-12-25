10b933ff0efbe1e634244b800504fadc3baa39f3
snailxxd <2559572532@qq.com>
2025-12-25 18:20:28 +0800

    feat: add map from playerId to ClientHandler

--- src/main/java/com/example/service/manager/PlayerManager.java
+++ src/main/java/com/example/service/manager/PlayerManager.java
@@ -1,6 +1,7 @@
 package com.example.service.manager;
 
 import com.example.model.player.Player;
+import com.example.service.server.ClientHandler;
 
 import java.util.Map;
 import java.util.UUID;
@@ -10,10 +11,11 @@ public class PlayerManager {
     private static final PlayerManager INSTANCE = new PlayerManager();
 
     private final Map<String, Player> players = new ConcurrentHashMap<>();
+    private final Map<String, ClientHandler> handlerMap = new ConcurrentHashMap<>();
 
     private PlayerManager() {}
 
-    public PlayerManager getInstance() {
+    public static PlayerManager getInstance() {
         return INSTANCE;
     }
 
@@ -33,6 +35,13 @@ public class PlayerManager {
     }
 
     /**
+     * 记录 ClientHandler
+     */
+    public void registerClientHandler(Player player, ClientHandler clientHandler) {
+        handlerMap.put(player.getId(), clientHandler);
+    }
+
+    /**
      * 查找玩家
      */
     public Player getPlayer(String id) {
@@ -40,10 +49,18 @@ public class PlayerManager {
     }
 
     /**
+     * 获取玩家对应的 ClientHandler
+     */
+    public ClientHandler getHandler(String id) {
+        return handlerMap.get(id);
+    }
+
+    /**
      * 移除玩家
      */
     public void removePlayer(String id) {
         players.remove(id);
+        handlerMap.remove(id);
         System.out.println("Player removed: " + id);
     }
 }
